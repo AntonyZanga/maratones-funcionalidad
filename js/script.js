fbbaf8fb529ca8af9a00d1e6a4d8f06e6ae63976
AntonyZanga <antonyzanga@gmail.com>
2025-03-18 18:32:06 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// js/script.js
+
+// Escucha el clic en el botón "Inscribirse" y redirige a la página de registro
+document.getElementById("btn-inscribirse").addEventListener("click", function() {
+  window.location.href = "registration.html";
+});
