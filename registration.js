0d85d35b998edc09e6fb4422905f2173a1b6e0c9
AntonyZanga <antonyzanga@gmail.com>
2025-03-21 15:50:48 -0300

    Update registration.js

--- registration.js
+++ registration.js
@@ -60,8 +60,11 @@ async function registrarAtleta() {
   let fechaNacimiento = document.getElementById("fecha-nacimiento").value;
   let localidad = document.getElementById("localidad").value.trim();
   let selectGrupo = document.getElementById("tipo-grupo");
-  let tipoGrupo = selectGrupo.value.trim() || "Individual";
-  let nombreGrupo = tipoGrupo === "grupo" ? document.getElementById("nombre-grupo").value.trim() : null;
+  let tipoGrupo = selectGrupo.value.trim();
+  let nombreGrupo = tipoGrupo ? tipoGrupo : "";
+  if (!tipoGrupo) {
+    tipoGrupo = "Individual";  // Si no elige un grupo, se asigna "Individual"
+  }
   let categoria = document.querySelector('input[name="categoria"]:checked')?.value;
   let password = document.getElementById("password").value;
   let confirmPassword = document.getElementById("confirm-password").value;
