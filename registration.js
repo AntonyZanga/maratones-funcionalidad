ab31573b93f833801c8996fa1f4963be31a7c112
AntonyZanga <antonyzanga@gmail.com>
2025-03-21 14:47:06 -0300

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,8 +1,7 @@
 // Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getFirestore, doc, getDoc, setDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 // Configuración de Firebase
 const firebaseConfig = {
