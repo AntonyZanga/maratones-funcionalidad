80d022544e8e5aed4fb48677fc36abd41405284e
AntonyZanga <antonyzanga@gmail.com>
2025-03-22 06:49:55 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,6 @@
 // Importar Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAFHZcfSELn2Cfgh3I1og2mw3rIL8gqlAM",
-  authDomain: "maratonessudeste.firebaseapp.com",
-  projectId: "maratonessudeste",
-  storageBucket: "maratonessudeste.appspot.com",
-  messagingSenderId: "76996108214",
-  appId: "1:76996108214:web:036e55fbfd01e15b462b17",
-  measurementId: "G-B1GL7QJGSH"
-};
-
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { auth, db } from './config.js';
+import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 // Función para mostrar mensajes de estado
 function mostrarMensaje(mensaje, color = "red") {
