04623c6e8521a40583deb05abfbd7c73321e6558
AntonyZanga <antonyzanga@gmail.com>
2025-03-22 07:05:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,27 @@
+// Importar las funciones necesarias de los SDKs de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
+
+// Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAFHZcfSELn2Cfgh3I1og2mw3rIL8gqlAM",
-  authDomain: "maratonessudeste.firebaseapp.com",
-  projectId: "maratonessudeste",
-  storageBucket: "maratonessudeste.appspot.com",
-  messagingSenderId: "76996108214",
-  appId: "1:76996108214:web:036e55fbfd01e15b462b17",
-  measurementId: "G-B1GL7QJGSH"
+    apiKey: "AIzaSyAFHZcfSELn2Cfgh3I1og2mw3rIL8gqlAM",
+    authDomain: "maratonessudeste.firebaseapp.com",
+    projectId: "maratonessudeste",
+    storageBucket: "maratonessudeste.appspot.com",
+    messagingSenderId: "76996108214",
+    appId: "1:76996108214:web:036e55fbfd01e15b462b17",
+    measurementId: "G-B1GL7QJGSH"
 };
 
-// Inicializa Firebase una sola vez
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
 
-// Exportamos para que otros archivos lo usen
-export { auth, db };
+// Inicializar servicios de Firebase
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Exportar los servicios para usarlos en otros archivos
+export { auth, db, storage };
