0413c290bbd76e0a9ce056487d9eb3969774ba74
AntonyZanga <antonyzanga@gmail.com>
2025-04-17 14:28:08 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// js/script.js
-
-// Escucha el clic en el botón "Inscribirse" y redirige a la página de registro
-document.getElementById("btn-inscribirse").addEventListener("click", function() {
-  window.location.href = "registration.html";
-});
